5d3ff83da0019dd85b66a7e9988b3e7e91ce3a83
hrim <>
2022-11-01 13:34:44 +0900

    초기셋팅

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+function Home() {
+  return (
+    <div>
+      <div className="container">
+        <div className="catecory-area">
+          <p>원하는 카테고리를 선택해주세요.</p>
+          <select name="">
+            <option></option>
+          </select>
+        </div>
+        <div className="people-num-area">
+          <input />
+        </div>
+        <div className="button-area">
+          <button>start</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
